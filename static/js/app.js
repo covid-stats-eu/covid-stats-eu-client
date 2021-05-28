4db17c543727a348f4407801d727eb41980f191b
dimprama <dev@dimitrisprama.com>
2021-05-28 12:22:46 +0300

    Added requests for A & B

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,4 @@
+// Tabs / accordion JS
 const labels = document.querySelectorAll(".accordion-item__label");
 const tabs = document.querySelectorAll(".accordion-tab");
 
@@ -40,13 +41,81 @@ tabs.forEach(function(tab) {
 	tab.addEventListener("click", toggleShow);
 });
 
+// ----------
+// Actions
+// ----------
 
+// Action A
 const action_A = (event) => {
   event.preventDefault()
-  const queryString = window.location.search
   
-  fetch(`http://localhost:3000/activity/getAllByCountry${queryString}`)
+  // Get Elements
+  const table = document.getElementById("result")
+  const form = document.getElementById("formA")
+  const fd = new FormData(form);
+  
+  // Get data
+  const countryParam = fd.get("country")
+  
+  // Construct url parameters
+  const params = new URLSearchParams({
+    country: countryParam,
+  }); 
+
+  // Construct url
+  const url = "http://localhost:3000/activity/getAllByCountry?" + params
+
+  // Request
+  fetch(url)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+      console.log(data[0])
+      table.innerHTML = `<tr>
+            <th>Deaths</th>
+            <th>Cases</th>\
+            <th>Tests</th>\
+          </tr>\
+          <tr>\
+            <td>${data[0]["deaths"]}</td>\
+            <td>${data[0]["cases"]}</td>\
+            <td>${data[0]["tests"]}</td>\
+          </tr>`
+    })
+    .catch(err => alert(err))
+}
+
+// Action B
+const action_B = (event) => {
+  event.preventDefault()
+  
+  // Get Elements
+  const table = document.getElementById("result")
+  const form = document.getElementById("formB")
+  const fd = new FormData(form);
+  
+  // Construct url parameters
+  const params = new URLSearchParams({
+    country:  fd.get("country"),
+    start:    fd.get("period-start"),
+    end:      fd.get("period-end"),
+  }); 
+
+  // Construct url
+  const url = "http://localhost:3000/activity/getDeathsAndCasesByDate?" + params
+
+  // Request
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      table.innerHTML = `<tr>
+            <th>Deaths</th>
+            <th>Cases</th>\
+          </tr>\
+          <tr>\
+            <td>${data[0]["deaths"]}</td>\
+            <td>${data[0]["cases"]}</td>\
+          </tr>`
+    })
     .catch(err => alert(err))
 }
